deaf21220f6a321e0b7da2375715758e586d805e
Stephan Hanzlik <stephanhanzlik@elements-mac114.local>
2019-01-13 09:04:18 -0700

    roughly built out migrationb

--- migrations/20190113080822_station.js
+++ migrations/20190113080822_station.js
@@ -1,5 +1,36 @@
 
 exports.up = function(knex, Promise) {
+
+    return Promise.all([
+
+        knex.schema.createTable('stationData', function(table){
+            table.increments('sdid').primary();
+            table.string('Date');
+            table.string('Snow_Water_Equivalent_in');
+            table.string('Change_In_Snow_Water_Equivalent_in');
+            table.string('Snow_Depth_in');
+            table.string('Change_In_Snow_Depth_in');
+            table.string('Air_Temperature_Observed_degF');
+        }),
+
+        knex.schema.createTable('station', function(table){
+            table.increments('sid').primary();
+            table.integer('elevation');
+            table.integer('location');
+            table.string('name');
+            table.integer('timezone');
+            table.string('triplet');
+            table.boolean('wind');
+        }),
+
+        knex.schema.createTable('location', function(table){
+            table.increments('lid').primary();
+            table.integer('lat');
+            table.integer('lng');
+        })
+
+
+    ])
   
 };
 
