5465b9634f47bf811202dcda5fc67e1ce56e187f
Stephen Hanzlik <stephenhanzlik@gmail.com>
2019-12-29 16:19:01 -0700

    added cors middleware to server

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,14 @@ module Enos
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # Access-Control-Allow-Origin
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'localhost:3001', /https*:\/\/.*?bloopist\.com/
+        resource '*', :headers => :any, :methods => :any
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
